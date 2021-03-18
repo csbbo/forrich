3c1ad546083433b9b155e7221d75c61653beb264
shaobo.chen <shaobo.chen@chaitin.com>
2021-03-18 16:31:24 +0800

    refactor

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from flask_apscheduler import APScheduler
-import tushare
+
 from pypinyin import Style, pinyin
+from concurrent.futures import ThreadPoolExecutor
+
+import tushare
+import datetime
+import time
+import base64
+import logging
 
 
+logger = logging.getLogger(__name__)
+
+# ================ init
 class Config:
     SCHEDULER_API_ENABLED = True
+    SCHEDULER_TIMEZONE = 'Asia/Shanghai'
     MONGO_URI = "mongodb://127.0.0.1:27017/forrich"
+    # MONGO_URI = "mongodb://forrich_mongo:27017/forrich"
 
     DEBUG = True
     HOST = '0.0.0.0'
     PORT = '9000'
-    TU_SHARE_TOKE = '595b56a214096d039f53fc2a4c3779a2217bd504eb1f0b9a24b07991'
+    TU_SHARE_TOKE = 'NTk1YjU2YTIxNDA5NmQwMzlmNTNmYzJhNGMzNzc5YTIyMTdiZDUwNGViMWYwYjlhMjRiMDc5OTE='
 
 
 app = Flask(__name__)
@@ -24,11 +36,109 @@ scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.start()
 
-ts = tushare.pro_api(Config.TU_SHARE_TOKE)
+pro = tushare.pro_api(str(base64.b64decode(bytes(Config.TU_SHARE_TOKE, encoding='utf-8')), encoding='utf-8'))
 
 
-@app.route('/test', methods=['GET', 'POST'])
-def get_user():
+# ================ class and function
+class Macd():
+    def __init__(self, price_list, slow_ema_n, quick_ema_n, dif_n):
+        self.price_list = price_list
+        self.quick_ema_n = quick_ema_n
+        self.slow_ema_n = slow_ema_n
+        self.dif_n = dif_n
+
+    def cal_ema(self, price_list, n):
+        ema_list = []
+        for index, price in enumerate(price_list):
+            if index == 0:
+                ema_list.append(price)
+            else:
+                pre_ema = ema_list[index-1]
+                ema_list.append((price * 2 / (n+1)) + (pre_ema * (n-1) / (n+1)))
+        return ema_list
+
+    def cal_dif(self, price_list, slow_ema_n, quick_ema_n):
+        slow_ema_list = self.cal_ema(price_list, slow_ema_n)
+        quick_ema_list = self.cal_ema(price_list, quick_ema_n)
+        dif_list = []
+        for i in range(len(price_list)):
+            dif_list.append(quick_ema_list[i] - slow_ema_list[i])
+        return dif_list
+
+    def get_dif_dea(self, price_list, slow_ema_n, quick_ema_n, dif_n):
+        dif_list = self.cal_dif(price_list, slow_ema_n, quick_ema_n)
+        dea_list = self.cal_ema(dif_list, dif_n)
+        return dif_list, dea_list
+
+    def pass_filter(self):
+        dif_list, dea_list = self.get_dif_dea(self.price_list, self.slow_ema_n, self.quick_ema_n, self.dif_n)
+        if len(dif_list) < 20:
+            return False
+
+        if dea_list[-2] < dif_list[-2] and dea_list[-1] > dif_list[-1] and abs(dea_list[-1]) < 0.05:
+            return True
+        return False
+
+
+def save_stocks():
+    fields = ['ts_code', 'symbol', 'name', 'area', 'industry', 'fullname', 'enname', 'market', 'exchange', 'curr_type', 'list_status', 'list_date', 'delist_date', 'is_hs']
+    fields_str = ','.join(fields)
+    df = pro.query('stock_basic', exchange='', list_status='L', fields=fields_str)
+
+    def short_pinyin(name):
+        s = ''
+        for letter in pinyin(name, style=Style.FIRST_LETTER, strict=False):
+            s += letter[0][0]
+        return s
+
+    data = {}
+    for _, row in df.iterrows():
+        for field in fields:
+            data[field] = row[field]
+        data['pinyin'] = short_pinyin(row['name'])
+        mongo.db.stocks.update_one({'ts_code': row['ts_code']}, {'$set': data}, upsert=True)
+
+
+def save_stock_price_history(ts_code, start_date, end_date):
+    df = pro.daily(ts_code=ts_code, start_date=start_date, end_date=end_date, fields='trade_date,close')
+
+    price = df['close'].to_list()
+    price.reverse()
+
+    trade_date = df['trade_date'].to_list()
+    trade_date.reverse()
+
+    data = {
+        'trade_date': trade_date,
+        'price': price
+    }
+    mongo.db.history.update_one({'ts_code': ts_code}, {'$set': data}, upsert=True)
+
+
+def save_all_stocks_price_history():
+    today = datetime.date.today().strftime('%Y%m%d')
+    df = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,list_date')
+    pool = ThreadPoolExecutor(max_workers=60)
+    for index, row in df.iterrows():
+        ts_code = row['ts_code']
+        list_date = row['list_date']
+        pool.submit(save_stock_price_history, ts_code, list_date, today)
+        if (index+1) % 500 == 0:    # The tushare interface is limited to 500 requests per minute
+            time.sleep(62)
+
+
+# ================ scheduler
+@scheduler.task('cron', id='scheduler_task', day_of_week='mon-fri', hour=15, minute=30)
+def scheduler_task():
+    now_time = (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d: %H:%M:%S')
+    print(f'task run in {now_time}')
+    save_stocks()
+    save_all_stocks_price_history()
+
+
+# ================ route
+@app.route('/demo', methods=['GET', 'POST'])
+def demo():
     args = request.args
     json = request.json
     method = request.method
@@ -46,58 +156,41 @@ def get_user():
 @app.route('/', methods=['GET'])
 def query():
     search = request.args.getlist('s')
-    stocks = mongo.db.stocks.find({'$or': [
-        {'short_pinyin': {'$in': search}},
+    filter = {'$or': [
+        {'pinyin': {'$in': search}},
         {'name': {'$in': search}},
-        {'code': {'$in': search}},
-    ]})
+        {'ts_code': {'$in': search}},
+        {'symbol': {'$in': search}},
+    ]}
+    stocks = mongo.db.stocks.find(filter)
 
     resp = []
     for stock in stocks:
-        df = tushare.get_realtime_quotes(stock.get('code'))
+        df = tushare.get_realtime_quotes(stock.get('symbol'))
         price = float(df.at[0, 'price'])
         pre_close = float(df.at[0, 'pre_close'])
-        resp.append(
-            {
-                # 'name': stock.get('name'),
-                'price': price,
-                'rose': f"{round((price - pre_close) / pre_close * 100, 3)}%",
-            }
-        )
+        rose = f"{round((price - pre_close) / pre_close * 100, 3)}%"
+        resp.append({'price': price, 'rose': rose})
     return str(resp)
 
 
-@scheduler.task('cron', id='update_stocks', day_of_week='*', hour=9, minute=15, second=0)
-def update_stocks_info():
-    fields = 'ts_code,symbol,name,area,industry,fullname,enname,market,exchange,curr_type,list_status,list_date,' \
-             'delist_date,is_hs '
-    df = ts.query('stock_basic', exchange='', list_status='L', fields=fields)
+@app.route('/pick', methods=['GET'])
+def self_selection():
+    content = ""
+    historys = mongo.db.history.find()
+    for history in historys:
+        price_list = history.get('price')
+        ts_code = history.get('ts_code')
+        macd = Macd(price_list, 26, 12, 9)
+        if macd.pass_filter():
+            content += f'<tr><td>{ts_code}</td></tr>'
+    return f'<table style="width:20px; margin:auto">{content}</table>'
 
-    def short_pinyin(name):
-        s = ''
-        for letter in pinyin(name, style=Style.FIRST_LETTER, strict=False):
-            s += letter[0][0]
-        return s
 
-    for row in df.itertuples():
-        item = {
-            'ts_code': row[1],
-            'code': row[2],
-            'name': row[3],
-            'short_pinyin': short_pinyin(row[3]),
-            'area': row[4],
-            'industry': row[5],
-            'fullname': row[6],
-            'enname': row[7],
-            'market': row[8],
-            'exchange': row[9],
-            'curr_type': row[10],
-            'list_status': row[11],
-            'list_date': row[12],
-            'delist_date': row[13],
-            'is_hs': row[14],
-        }
-        mongo.db.stocks.update_one({'code': item['code']}, {'$set': item}, upsert=True)
+@app.route('/task', methods=['GET'])
+def get_price_history():
+    save_stocks()
+    save_all_stocks_price_history()
     return jsonify({"resp": "success"})
 
 
